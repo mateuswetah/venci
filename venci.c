6c7e6a934a2e734821feba38e6192209e2fa42ef
Leonardo Pacheco <leonardovpacheco@gmail.com>
2012-12-12 16:08:05 -0200

    Update venci.c
    Telas refeitas.
    Comentado.
    Falta texto de ajuda.

--- venci.c
+++ venci.c
@@ -1,103 +1,179 @@
 #include <ncurses.h>
-#include <string.h>
-
-
-void menu(WINDOW *win);  
-void salvar();
-void abrir();
-void novo_arquivo();
-void ajuda();
-int quit = 0;
-
-int main(){ 
+void novo(WINDOW *edit, WINDOW *bar){
 	
-  	char c;
-	char splash[4][50]={	"BEM VINDO AO VENCI!",
-				"An Epic Epic of Epicness Text Editor",
-				"Aperte ESC para acessar o menu",
-				"Utilize o comando :h para ajuda..."
-			};
-
-	initscr();
-	cbreak();
-	noecho();
+	// Limpa a tela
+	werase(edit);
+	wrefresh(edit);
 	
-	//Mostra a splash screen
-	//Quando for implementada a janela de edição irá para uma função separada.
-	attron(A_BOLD);
-	mvprintw((LINES/2)-2, (COLS-strlen(splash[0]))/2,"%s",splash[0]);
-	attroff(A_BOLD);
-	attron(A_UNDERLINE);
-	mvprintw((LINES/2)-1, (COLS-strlen(splash[1]))/2,"%s",splash[1]);
-	attroff(A_UNDERLINE);
-	mvprintw(LINES/2, (COLS-strlen(splash[2]))/2,"%s",splash[2]);
-	mvprintw((LINES/2)+1, (COLS-strlen(splash[3]))/2,"%s",splash[3]);
-	refresh();
-	move(0,0);
+}
+void salvar(){}
+void abrir(){}
+void salvar_como(){}
+void ajuda(WINDOW *menu, WINDOW *bar) {
 	
-	WINDOW *win;
-	win = newwin(2, COLS, LINES-2, 0);
+	int i, j;
 	
-	menu(win);
+	// Limpa a tela
+	werase(menu);
+	wrefresh(menu);
 	
-	if(quit == 1){
-		delwin(win);
-		endwin();
-		return 0;
-	}
-	
-	//Loop principal
-	while((c = getch())){
-		if(c == 27){
-			//Se for apertado ESC vai para o menu
-			menu(win);
-			if(quit == 1){
-				break;
-			}
-		}else{
-			printw("%c",c);
+	// Imprime a ajuda
+	box(menu, 0, '\0');	
+	for (i = 1; i < 19; i++) {
+		for (j = 1; j < 29; j++) {
+			mvwaddch(menu, i, j, ' ');
 		}
-		refresh();
+	}	
+	mvwprintw(menu, 1, 2, "Ajuda");
+	mvwprintw(menu, 3, 2, "Adhoc");
+	
+	// Organiza a barra
+	werase(bar);
+	wrefresh(bar);
+	mvwprintw(bar, 1, 0, "Pressione qualquer tecla para voltar ao editor.");
+	
+	// Exibe a ajuda
+	wrefresh(bar);
+	wrefresh(menu);
+	
+	getch();
+				
+	werase(bar);
+	wrefresh(bar);
+
+}
+
+int show_menu(WINDOW *menu, WINDOW *bar, WINDOW *edit) {
+	
+	//Inicialização
+	curs_set(0);
+	
+	int pos = 4;
+	int ch;
+	
+	// Colocar bordas na menu
+	box(menu, 0, '\0');
+	
+	// Imprime o menu
+	mvwprintw(menu, 2, 12, "VENCI");
+	mvwprintw(menu, 4,5,"novo");
+	mvwprintw(menu, 5,5,"abrir");
+	mvwprintw(menu, 6,5,"salvar");
+	mvwprintw(menu, 7,5,"salvar como");
+	mvwprintw(menu, 8,5,"ajuda");
+	mvwprintw(menu, 9,5,"sair");
+	mvwaddch(menu, pos, 3, '>');
+	
+	wrefresh(menu);
+	
+	
+	// Movimentação do cursor e sair do menu
+	while ((ch = getch())){
+		mvwaddch(menu, pos, 3, ' ');
+		if (ch == KEY_DOWN){
+			if (pos >=4 && pos <=8){
+				pos++;
+			}else if (pos == 9){
+				pos = 4;
+			}
+		}else if (ch == KEY_UP){
+			if (pos >=5 && pos <=9){
+				pos--;
+			}else if (pos == 4){
+				pos = 9;
+			}
+		}else if (ch == 10){ // ENTER para escolher opção
+			break;
+		}else if (ch == 27){ // ESC para sair do menu sem selecionar
+			return 0;
+		}
+		
+		mvwaddch(menu, pos, 3, '>');
+		wrefresh(menu);
 	}
 	
-	delwin(win);
-	endwin();
+	werase(bar);
+	wrefresh(bar);
+	
+	// Executa a função de acordo com a opção escolhida
+	switch(pos) {		
+		case 4: novo(edit, bar);
+			mvwprintw(bar, 1, 0, "Função novo não implementada");
+			break;
+		case 5: abrir();
+			mvwprintw(bar, 1, 0, "Função abrir não implementada");
+			break;	
+		case 6: salvar();
+			mvwprintw(bar, 1, 0, "Função salvar não implementada");
+			break;	
+		case 7: salvar_como();
+			mvwprintw(bar, 1, 0, "Função salvar como não implementada");
+			break;	
+		case 8: ajuda(menu, bar);
+			break;
+		case 9:
+			return 1;
+			break;
+	}
+	wrefresh(bar);
+
+	curs_set(1);
 	return 0;
 }
 
-void menu(WINDOW *win){
+int main() {
 	
-	//Alterna a janela para o MENU
-	char option[6];
-	werase(win);
-	mvwaddstr(win, 0, 0, "-------------------------MODO-MENU----------------------------------------------\n");
-	wrefresh(win);
-	
-	echo();
-	
-	mvwscanw(win,1,0,"%s", option);
-	
-	if (strcmp(option,"i")==0){
-		werase(win);
-	}else if (strcmp(option,":q")==0){
-		quit = 1;
-	}else if (strcmp(option,":w")==0){
-		wprintw(win,"Função salvar ainda não implementada!");
-	}else if (strcmp(option,":e")==0){
-		wprintw(win,"Função abrir arquivo ainda não implementada!");
-	}else if (strcmp(option,":enew")==0){
-		wprintw(win,"Função novo arquivo ainda não implementada!");
-	}else if (strcmp(option,":h")==0){	
-		erase();
-		refresh();
-		printw("YOU KNOW I NEED SOMEONE...HEEEELP!!!!!\n\nHá dois modos no editor: MENU e INSERIR.\nO programa inicia no MODO MENU.\nPara inserir, tecle i e dê ENTER.\nPara ir para o menu, tecle ESC. Digite um dos comandos a seguir:\n----> :e --- abre um arquivo;\n----> :enew --- abre um novo arquivo;\n----> :w --- salva um arquivo;\n----> :q --- sair do programa;\n----> :h --- ajuda;\n");
-	}else{
-		wprintw(win,"Comando inválido");
-	}
-	
-	//Volta para o modo INSERIR
-	mvwaddstr(win, 0, 0,"--------------------------INSERIR-----------------------------------------------\n");
-	wrefresh(win);
-	
+	// Inicia a ncurses
+	initscr();
 	noecho();
+	cbreak();
+
+	// Cria as  janelas
+	WINDOW *menu;
+	WINDOW *edit;
+	WINDOW *bar;
+		
+	menu = newwin(20, 30, (LINES/2)-10, (COLS/2)-15);
+	edit = newwin(LINES-2, COLS, 0, 0);
+	bar = newwin(2, COLS, LINES-2, 0);
+	
+	// Habilita teclas especiais
+	keypad(stdscr, TRUE);
+	keypad(menu, TRUE);
+
+	// Refresh para plotar todas as janelas
+	refresh();
+	
+	// Inicializa as variáveis da main
+	short init = 0;
+	char ch;	
+	
+	// Loop principal
+	while (1) {
+		if (ch == 27 || init == 0) { // ESC ou primeira execução do loop para exebir o menu
+			if(show_menu(menu, bar, edit) == 1){
+				break;
+			}
+		
+			init = 1;
+			
+			werase(menu);
+			wrefresh(menu);
+			
+			wrefresh(edit);
+			wmove(edit, 0, 0);
+		}
+		
+		ch = getch();
+		
+		if (ch != 27) {
+			waddch(edit, ch);
+			wrefresh(edit);
+		}
+		
+	}
+
+	endwin();
+		 
+	return 0;
 }
