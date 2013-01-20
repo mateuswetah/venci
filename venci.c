bcb1fd906568d35d08e29ea1cc8a01ce4c38be4f
Mateus Machado Luna <mateus.m.luna@gmail.com>
2013-01-20 21:46:30 -0200

    Update venci.c
    Adicionada a panel.h com a implementação do painel para o menu popup;
    Adicionadas funcionalidades parciais de salvar, abrir, salvar como e novo;
    Adiconadas cores básicas do menu e barra de informações.

--- venci.c
+++ venci.c
@@ -1,19 +1,31 @@
 /**
  * VENCI - https://github.com/mateuswetah/venci
  * Alunos: Eduardo Faria de Souza,
- *         Leonardo Vieira Pacheco,
- *         Mateus Machado Luna
+ * 		   Leonardo Vieira Pacheco,
+ * 		   Mateus Machado Luna
  */
 
 #include <ncurses.h>
+#include <panel.h>
+#include <string.h>
 
 //Protótipos de função
 void novo(WINDOW *edit, WINDOW *bar);
-void salvar();
-void abrir();
-void salvar_como();
+void salvar(WINDOW *edit, WINDOW *bar);
+void abrir(WINDOW *edit, WINDOW *bar);
+void salvar_como(WINDOW *edit, WINDOW *bar);
 void ajuda(WINDOW *menu, WINDOW *bar, WINDOW *edit);
 int show_menu(WINDOW *menu, WINDOW *bar, WINDOW *edit);
+
+//Cria os painéis
+PANEL *popup;
+PANEL *editor;
+PANEL *infobar;
+
+//Algumas variáveis globais
+FILE *text;
+char nome[30]; // Nome do arquivo
+int count = 0; // Registra se é a primeira vez que um documento é aberto.
 
 int main() {
 	
@@ -21,6 +33,7 @@ int main() {
 	initscr();
 	noecho();
 	cbreak();
+	start_color();
 
 	// Cria as  janelas
 	WINDOW *menu;
@@ -31,28 +44,39 @@ int main() {
 	edit = newwin(LINES-2, COLS, 0, 0);
 	bar = newwin(2, COLS, LINES-2, 0);
 	
+	//Associa os painéis às respectivas janelas
+	editor = new_panel(edit);
+	infobar = new_panel(bar);
+	popup = new_panel(menu);
+
+	//Atributos
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+	wattrset(bar, COLOR_PAIR(1));
+	wattrset(menu, COLOR_PAIR(1));
+	
 	// Habilita teclas especiais
 	keypad(stdscr, TRUE);
 	keypad(menu, TRUE);
 
 	// Refresh para plotar todas as janelas
 	refresh();
+	update_panels();
+	doupdate();	
 	
 	// Inicializa as variáveis da main
-	short init = 0;
-	char ch;	
+	char ch = 27;	
+	strcpy (nome, "novo_arquivo");
 	
 	// Loop principal
 	while (1) {
-		if (ch == 27 || init == 0) { // ESC ou primeira execução do loop para exebir o menu
+		if (ch == 27) { // ESC para exebir o menu
 			if(show_menu(menu, bar, edit) == 1){
 				break;
 			}
-		
-			init = 1;
 			
-			werase(menu);
-			wrefresh(menu);
+			hide_panel(popup);
+			update_panels();
+			doupdate();
 			
 			wrefresh(edit);
 			wmove(edit, 0, 0);
@@ -71,17 +95,196 @@ int main() {
 		 
 	return 0;
 }
+
+int show_menu(WINDOW *menu, WINDOW *bar, WINDOW *edit) {
+	
+	//Ajusta o painel
+	show_panel(popup);
+	update_panels();
+	doupdate();	
+	
+	//Inicialização
+	curs_set(0);
+	
+	int pos = 4;
+	int ch;
+	
+	// Colocar bordas na menu
+	box(menu, 0, '\0');
+	
+	// Imprime o menu
+	mvwprintw(menu, 0, 12, "VENCI");
+	mvwprintw(menu, 4, 5, "novo");
+	mvwprintw(menu, 5, 5, "abrir");
+	mvwprintw(menu, 6, 5, "salvar");
+	mvwprintw(menu, 7, 5, "salvar como");
+	mvwprintw(menu, 8, 5, "ajuda");
+	mvwprintw(menu, 10, 5, "sair");
+	mvwaddch(menu, pos, 3, '>');
+	
+	wrefresh(menu);
+	
+	// Movimentação do cursor e sair do menu
+	while ((ch = getch())){
+		mvwaddch(menu, pos, 3, ' ');
+		if (ch == KEY_DOWN){
+			if (pos >=4 && pos <=8){
+				pos++;
+				if (pos == 9) {
+					pos++;
+				}		
+			}else if (pos == 10){
+				pos = 4;
+			}
+		}else if (ch == KEY_UP){
+			if (pos >=5 && pos <=10){
+				pos--;
+				if (pos == 9) {
+					pos--;
+				}
+			}else if (pos == 4){
+				pos = 10;
+			}
+		}else if (ch == 10){ // ENTER para escolher opção
+			break;
+		}else if (ch == 27){ // ESC para sair do menu sem selecionar
+			return 0;
+		}
+		
+		mvwaddch(menu, pos, 3, '>');
+		wrefresh(menu);
+		
+	}
+	
+	werase(bar);
+	wrefresh(bar);
+	
+	// Executa a função de acordo com a opção escolhida
+	switch(pos) {		
+		case 4: novo(edit, bar);
+			break;
+		case 5: abrir(edit, bar);
+			break;	
+		case 6: salvar(edit, bar);
+			break;	
+		case 7: salvar_como(edit, bar);
+			break;	
+		case 8: ajuda(menu, bar,edit);
+			break;
+		case 10:
+			return 1;
+			break;
+	}
+	wrefresh(bar);
+
+	curs_set(1);
+		
+	count++; // Já acessou o menu mais de uma vez, então já acesssou o arquivo.
+	
+	return 0;
+}
+
 void novo(WINDOW *edit, WINDOW *bar){
+	
+	char ch;
+	
+	//Verifica se Deseja salvar o arquivo atual antes de criar um novo;
+	if (count > 0) {  //só pergunta se o arquivo já tiver sido acessado.
+		werase(bar);
+		wrefresh(bar);
+		wprintw(bar, "Deseja salvar e sair do arquivo aberto? (S/N)");
+		echo();
+		mvwscanw(bar, 1, 0, "%c", &ch);
+		noecho();
+		
+		if (ch == 'S') {
+			
+			salvar(edit, bar);
+			
+		} else if (ch == 'N') {
+			
+		}
+		
+	}
+	
+	werase(bar);
+	strcpy (nome, "novo_arquivo");
+	mvwprintw(bar, 0, 0, "Arquivo %s criado.", nome);
 	
 	// Limpa a tela
 	werase(edit);
 	wrefresh(edit);
+	
 }
 
-void salvar(){}
-void abrir(){}
-void salvar_como(){}
+void abrir(WINDOW *edit, WINDOW *bar) {
+	
+	mvwprintw(bar, 0, 0, "Entre com o nome do arquivo que vai abrir:");
+	wrefresh (bar);
+	echo();	
+	wattron(bar, A_BLINK);
+	mvwscanw(bar, 1, 0, "%s", nome);
+	noecho();
+	
+	char ch;
+	
+	if ((text = fopen(nome, "r")) == NULL) {
+		werase(bar);
+		mvwprintw(bar, 0, 0, "Não foi possível abrir o arquivo!\nAperte ESC para voltar ao menu.");
+		wrefresh(bar);
+		
+	} else {
+		
+		wclear(bar);
+		//Na InfoBar, devemos informar nome do documento aberto, última
+		//modificação, número de caractéres, etc... 
+		mvwprintw(bar, 0, 0, "Documento %s aberto. Pressione ESC para voltar ao Menu", nome);
+		
+		wmove (edit, 0, 0);
+		
+		while ((ch = fgetc(text)) != EOF) {
+					
+			wprintw(edit, "%c", ch);
+			
+		}
+		
+		fclose(text);
+		
+	}
+	
+	wrefresh (bar);
+	
+}
 
+void salvar(WINDOW *edit, WINDOW *bar) {
+	
+	werase(bar);
+	mvwprintw(bar, 0,0 , "Arquivo %s salvo!", nome);
+	
+	wrefresh(bar);
+	
+	if(strcmp(nome, "novo_arquivo") == 0) {
+		salvar_como (edit,bar);
+	} else {
+		
+		text = fopen(nome, "w");
+		//Rotina para salvar na memória cada ch que ele recebe.
+		
+	}
+	
+}
+	
+void salvar_como(WINDOW *edit, WINDOW *bar){
+	
+	mvwprintw(bar, 0, 0, "Entre com o nome do novo arquivo");
+	wrefresh (bar);
+	echo();	
+	mvwscanw(bar, 1, 0, "%s", nome);
+	noecho();
+	wclear(bar);
+	salvar(edit,bar);
+
+}
 
 void ajuda(WINDOW *menu, WINDOW *bar, WINDOW *edit) {
 	
@@ -135,87 +338,4 @@ void ajuda(WINDOW *menu, WINDOW *bar, WINDOW *edit) {
 	}
 }
 
-int show_menu(WINDOW *menu, WINDOW *bar, WINDOW *edit) {
-	
-	//Inicialização
-	curs_set(0);
-	
-	int pos = 4;
-	int ch;
-	
-	// Colocar bordas na menu
-	box(menu, 0, '\0');
-	
-	// Imprime o menu
-	mvwprintw(menu, 0, 12, "VENCI");
-	mvwprintw(menu, 4, 5, "novo");
-	mvwprintw(menu, 5, 5, "abrir");
-	mvwprintw(menu, 6, 5, "salvar");
-	mvwprintw(menu, 7, 5, "salvar como");
-	mvwprintw(menu, 8, 5, "ajuda");
-	mvwprintw(menu, 10, 5, "sair");
-	mvwaddch(menu, pos, 3, '>');
-	
-	wrefresh(menu);
-	
-	
-	// Movimentação do cursor e sair do menu
-	while ((ch = getch())){
-		mvwaddch(menu, pos, 3, ' ');
-		if (ch == KEY_DOWN){
-			if (pos >=4 && pos <=8){
-				pos++;
-				if (pos == 9) {
-					pos++;
-				}		
-			}else if (pos == 10){
-				pos = 4;
-			}
-		}else if (ch == KEY_UP){
-			if (pos >=5 && pos <=10){
-				pos--;
-				if (pos == 9) {
-					pos--;
-				}
-			}else if (pos == 4){
-				pos = 10;
-			}
-		}else if (ch == 10){ // ENTER para escolher opção
-			break;
-		}else if (ch == 27){ // ESC para sair do menu sem selecionar
-			return 0;
-		}
-		
-		mvwaddch(menu, pos, 3, '>');
-		wrefresh(menu);
-	}
-	
-	werase(bar);
-	wrefresh(bar);
-	
-	// Executa a função de acordo com a opção escolhida
-	switch(pos) {		
-		case 4: novo(edit, bar);
-			mvwprintw(bar, 1, 0, "Função novo não implementada");
-			break;
-		case 5: abrir();
-			mvwprintw(bar, 1, 0, "Função abrir não implementada");
-			break;	
-		case 6: salvar();
-			mvwprintw(bar, 1, 0, "Função salvar não implementada");
-			break;	
-		case 7: salvar_como();
-			mvwprintw(bar, 1, 0, "Função salvar como não implementada");
-			break;	
-		case 8: ajuda(menu, bar,edit);
-			break;
-		case 10:
-			return 1;
-			break;
-	}
-	wrefresh(bar);
-
-	curs_set(1);
-	return 0;
-}
 
