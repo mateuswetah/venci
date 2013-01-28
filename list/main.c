419f6fb08c2cef5baf2520c20bf772a0fc3b89ef
Eduardo Souza <eduardodx01@gmail.com>
2013-01-28 17:02:16 -0200

    Merged the files the get easy to work.

--- list/main.c
+++ list/main.c
@@ -1,13 +1,183 @@
 #include <stdio.h>
-#include "list.h"
+#include <stdlib.h>
+
+/* Link */
+typedef struct link link_t;
+
+struct link {
+    char data;
+    link_t * prev;
+    link_t * next;
+};
+
+/* Linked list */
+typedef struct linked_list {
+    link_t * first;
+    link_t * last;
+} linked_list_t;
+
+/* Linked list init */
+void linked_list_init(linked_list_t * list) 
+{    
+    list->first = list->last = 0;
+}
+
+/* Add */
+void linked_list_add(linked_list_t * list, char data) 
+{
+    link_t * link;
+    
+    /* calloc sets the "next" field to zero. */
+    link = calloc(1, sizeof(link_t));
+    
+    if (!link) {
+        fprintf(stderr, "calloc failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    link->data = data;
+    
+    if (list->last) {
+        /* Join the two final links together. */
+        list->last->next = link;
+        link->prev = list->last;
+        list->last = link;
+    } else {
+        list->first = link;
+        list->last = link;
+    }    
+}
+
+/* Delete */
+void linked_list_delete(linked_list_t * list, link_t * link)
+{
+    link_t * prev;
+    link_t * next;
+    
+    prev = link->prev;
+    next = link->next;
+    
+    if (prev) {
+        if (next) {
+            /* Both the previous and next links are valid, so just
+                bypass "link" without altering "list" at all */
+            prev->next = next;
+            next->prev = prev;
+        } else {
+            /* Only the previous link is valid, so "prev" is now the
+                last link in the "list". */
+            prev->next = 0;
+            list->last = prev;
+        }
+    } else {
+        if (next) {
+            /* Only the next link is valid, not the previous one, so
+                "next" is now the first link in "list". */
+            next->prev = 0;
+            list->first = next;
+        } else {
+            /* Neither previous nor next links are valid, so the list
+                is now empty. */
+            list->first = 0;
+            list->last = 0;
+        }
+    }
+    
+    free(link);
+}
+
+/* Traverse */
+void linked_list_traverse(linked_list_t * list, void (*callback) (char))
+{
+    link_t * link;
+    
+    for (link = list->first; link; link = link->next) {
+        callback(link->data);
+    }
+}
+
+/* Free list */
+void linked_list_free(linked_list_t * list)
+{
+    link_t * link;
+    link_t * next;
+    
+    for (link = list->first; link; link = next) {
+        /* Store the next value so that we don't access freed memory */
+        next = link->next;
+        free(link);
+    }
+}
+
+/* Print list */
+void print_list(char data)
+{
+    printf("%c", data);
+}
+
+/** LINE LINKED LIST **/
+
+/* Link */
+typedef struct line_link line_link_t;
+
+struct line_link {
+    linked_list_t * head;
+    line_link_t * prev;
+    line_link_t * next;
+};
+
+/* Linked list */
+typedef struct line_linked_list {
+    line_link_t * first;
+    line_link_t * last;
+} line_linked_list_t;
+
+/* Linked list init */
+void line_linked_list_init(line_linked_list_t * list)
+{
+    list->first = list->last = 0;
+}
+
+/* Add */
+void line_linked_list_add(line_linked_list_t * list, linked_list_t * head)
+{
+    line_link_t * link;
+    
+    /* calloc sets the "next" field tyo zero. */
+    link = calloc(1, sizeof(line_link_t));
+    
+    if (!link) {
+        fprintf(stderr, "calloc failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    link->head = head;
+    
+    if (list->last) {
+        /* Join the two final links together. */
+        list->last->next = link;
+        link->prev = list->last;
+        list->last = link;
+    } else {
+        list->first = link;
+        list->last = link;
+    }
+}
+
+/** END OF LINE LINKED LIST **/
 
 int main() {
     
+    line_linked_list_t lines;
     linked_list_t list;
+
+    line_linked_list_init(&lines);
     linked_list_init(&list);
     
+    line_linked_list_add(&lines, &list);
+    
     linked_list_add(&list, 'a');
-    linked_list_add(&list, 'b');
+    linked_list_add(&list, 'l');
     linked_list_add(&list, 'c');
     
     linked_list_traverse(&list, print_list);
