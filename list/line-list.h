dcb7ac2eab8677a053acd30c9b55602a9306ce73
Eduardo Souza <eduardodx01@gmail.com>
2013-01-28 16:40:08 -0200

    Added structs and list init and add.

--- list/line-list.h
+++ list/line-list.h
@@ -5,7 +5,7 @@
 typedef struct line_link line_link_t;
 
 struct line_link {
-    linked_list * head;
+    linked_list_t * head;
     line_link_t * prev;
     line_link_t * next;
 };
@@ -15,5 +15,37 @@ typedef struct line_linked_list {
     line_link_t * first;
     line_link_t * last;
 } line_linked_list_t;
+
+/* Linked list init */
+void line_linked_list_init(line_list_t * list)
+{
+    list->first = list->last = 0;
+}
+
+/* Add */
+void line_linked_list_add(line_linked_list * list, linked_list_t * head)
+{
+    line_link_t * link;
+    
+    /* calloc sets the "next" field tyo zero. */
+    link = calloc(1, sizeof(line_link_t));
+    
+    if (!link) {
+        fprinf(stderr, "calloc failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    link->head = head;
+    
+    if (list->last) {
+        /* Join the two final links together. */
+        list->last->next = link;
+        link->prev = list->last;
+        list->last = link;
+    } else {
+        list->first = link;
+        list->last = link;
+    }
+}
 
 #endif
